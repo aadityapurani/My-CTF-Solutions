6a9d310f13374249e63ec1eb7a4ee9c64b4ba9c7
Aaditya Purani <aaditya.purani@gmail.com>
2019-03-31 15:17:21 -0500

    Create solve.py

---
+++ volgactf2019/shadowcrack/solve.py
@@ -0,0 +1,7 @@
+hmap = {'x':'b', 'a':'a','_':'z','n':'n','u':'t','f':'j','o':'x','l':'w','d':'g','m':'q','z':'_','y':'r','c':'e','t':'o','e':'i','v':'h','p':'y','s':'d','g':'m','i':'v','r':'l','h':'p','b':'k','q':'c','k':'u','j':'s','w':'f'}
+flag=""
+for c in a:
+    flag+=hmap[c]
+print flag
+
+#pass_hash_cracking_hashcat_always_lurks_in_the_shadows
